a34b81dac40642566ed6781bdc29b52669ff3733
wenjiasheng <584479748@qq.com>
2022-01-10 23:53:13 +0800

    字母异位词分组 unordered_map

---
+++ src/字母异位词分组.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<vector>
+#include<unordered_map>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> mp;
+        for (string& str: strs) {
+            string key = str;
+            sort(key.begin(), key.end());
+            mp[key].push_back(str);
+        }
+        vector<vector<string>> ans;
+        for (auto it = mp.begin(); it != mp.end(); ++it) {
+            ans.push_back(it->second);
+        }
+        return ans;
+
+    }
+};
